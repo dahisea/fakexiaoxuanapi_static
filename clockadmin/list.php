19716a8c0bf41da14b14531e2f53930541eb1f41
Dahi <81472122+dahisea@users.noreply.github.com>
2024-08-13 17:04:40 +0800

    Create list.php

---
+++ clockadmin/list.php
@@ -0,0 +1,38 @@
+{
+    "totalpage": 1,
+    "page": "1",
+    "list": [
+        {
+            "id": "1",
+            "theme": "A",
+            "pr": "0",
+            "designer": "by mking",
+            "system": "andriodwear",
+            "class": "9",
+            "sn": "600",
+            "thumbnail": "534thumbnail.jpg",
+            "package": "1package.zip",
+            "online": "1",
+            "downnum": "636",
+            "usenum": "1835",
+            "time": "2024-06-23 09:52:00",
+            "payStatus": 1
+        },
+        {
+            "id": "2",
+            "theme": "B",
+            "pr": "0",
+            "designer": "by mking",
+            "system": "andriodwear",
+            "class": "9",
+            "sn": "577",
+            "thumbnail": "542thumbnail.jpg",
+            "package": "2package.zip",
+            "online": "1",
+            "downnum": "426",
+            "usenum": "1305",
+            "time": "2024-04-06 09:18:13",
+            "payStatus": 1
+        }
+    ]
+}
